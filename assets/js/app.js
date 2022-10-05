79aea516af3856da043c9bff6ffa033cfdf27d20
nahidthenh <nhgroup18@gmail.com>
2022-10-05 22:59:36 +0600

    NeedSomeFinalCheck

--- assets/js/app.js
+++ assets/js/app.js
@@ -18,3 +18,45 @@ $('.owl-carousel').owlCarousel({
         }
     }
 })
+
+
+
+var swiper6 = new Swiper(".mySwiper6", {
+    grabCursor: true,
+    effect: "creative",
+    creativeEffect: {
+        prev: {
+            shadow: true,
+            origin: "left center",
+            translate: ["-5%", 0, -200],
+            rotate: [0, 100, 0],
+        },
+        next: {
+            origin: "right center",
+            translate: ["5%", 0, -200],
+            rotate: [0, -100, 0],
+        },
+    },
+});
+
+
+
+
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function () { myFunction() };
+
+// Get the navbar
+var navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky")
+    } else {
+        navbar.classList.remove("sticky");
+    }
+} 

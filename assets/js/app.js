679292fb165e5fd453db1fc1040cd4ccc09528f8
nahidthenh <nhgroup18@gmail.com>
2022-10-05 17:31:19 +0600

    Sponsor Area Done

---
+++ assets/js/app.js
@@ -0,0 +1,20 @@
+
+// For Sponsor Logo 
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 0,
+    nav: false,
+    dot: false,
+    responsiveClass: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 3
+        },
+        1000: {
+            items: 5
+        }
+    }
+})
